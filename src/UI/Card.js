d7579e84e879ec6d6623496f89ca31a824cc92c3
Vimanshu Aryan <vimanshuaryan@gmail.com>
2023-05-29 00:21:57 +0530

    29-05-23

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Card.css';
 
 const Card = (props) => {
     return (
@@ -7,7 +8,7 @@ const Card = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{props.name}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href={props.url} className="btn btn-primary">Go somewhere</a>
+                    <a href={props.url} className="btn btn-primary">Know More</a>
                 </div>
         </div>
     )
