b4fac7827af2f5d48ad90d53658e3f1ff652a958
Vimanshu Aryan <vimanshuaryan@gmail.com>
2023-05-29 19:51:59 +0530

    layout and api calls complete

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import './Card.css';
 
 const Card = (props) => {
+
+    const [weight, setWeight] = useState(null);
+    const [height, setHeight] = useState(null);
+    const [stats, setStats] = useState(null);
+    const [type, setType] = useState(null);
+
+    const knowMoreHandler = () => {
+        const knowMore = async () => {
+            const response = await fetch(props.url);
+            const data = await response.json();
+            console.log(data);
+            setWeight(data.weight);
+            setHeight(data.height);
+            setStats(data.stats);
+            setType(data.types[0].type.name);
+        }
+        knowMore();
+    }
+
     return (
-        <div className="card" style={{width: '18rem'}}>
-            <img src="..." className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{props.name}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href={props.url} className="btn btn-primary">Know More</a>
-                </div>
+        <div className="card" style={type ? {width: '15rem', height: '20rem'} : { width: '15rem', height: '12rem' }}>
+            <div className="card-body">
+                <h6 className="card-title">{props.name}</h6>
+                <button onClick={knowMoreHandler} className="btn btn-primary">Know More</button>
+                {weight && <h6>weight - {weight}lbs</h6>}
+                {height && <h6>height - {height}cms</h6>}
+                {type && <h6>type - {type}</h6>}
+                {stats && stats.map((stat) => {
+                    return (
+                        <div key = {stat.stat.url}>
+                            <h6>{stat.stat.name} - {stat.base_stat}</h6>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 };
 
 export default Card;
+
+// In cases where you need to use other units, such as 'em' or 'rem', specify the unit with the value explicitly as a string. Applying that to the padding property should result in padding: '1.5em'. In addition, these styles are not vendor-prefixed automatically, so you have to add vendor prefixes manually.
--- src/components/PokeItem.js
+++ src/components/PokeItem.js
@@ -1,62 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import './PokeItem.css';
 
 const PokeItem = (props) => {
 
-    const data = {
-        "count": 1281,
-        "next": "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20",
-        "previous": null,
-        "results": [
-            {
-                "name": "bulbasaur",
-                "url": "https://pokeapi.co/api/v2/pokemon/1/"
-            },
-            {
-                "name": "ivysaur",
-                "url": "https://pokeapi.co/api/v2/pokemon/2/"
-            },
-            {
-                "name": "venusaur",
-                "url": "https://pokeapi.co/api/v2/pokemon/3/"
-            },
-            {
-                "name": "charmander",
-                "url": "https://pokeapi.co/api/v2/pokemon/4/"
-            },
-            {
-                "name": "charmeleon",
-                "url": "https://pokeapi.co/api/v2/pokemon/5/"
-            },
-            {
-                "name": "charizard",
-                "url": "https://pokeapi.co/api/v2/pokemon/6/"
-            },
-            {
-                "name": "squirtle",
-                "url": "https://pokeapi.co/api/v2/pokemon/7/"
-            },
-            {
-                "name": "wartortle",
-                "url": "https://pokeapi.co/api/v2/pokemon/8/"
-            },
-            {
-                "name": "blastoise",
-                "url": "https://pokeapi.co/api/v2/pokemon/9/"
-            }
-        ]
+    const [pokeData, setPokeData] = useState([]);
 
-    }
+    useEffect (() => {
+        const fetchPokemon = async () => {
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
+            const data = await response.json()
+            setPokeData(data.results);
+        }
+        fetchPokemon();
+    },[])
 
-    // async function fetchPokemon () {
-    //     const response = await fetch(url);
-    //     const data = await response.json();
-    // }
+    // todo
+    // Click on know more to get stats,height and weight
 
     return (
         <div className="poke-item">
-            {data.results.map((pokemon) => {
+            {pokeData.map((pokemon) => {
                 return <Card key={pokemon.url}
                     name={pokemon.name}
                     url={pokemon.url} />
@@ -67,4 +31,3 @@ const PokeItem = (props) => {
 
 export default PokeItem;
 
-// In cases where you need to use other units, such as 'em' or 'rem', specify the unit with the value explicitly as a string. Applying that to the padding property should result in padding: '1.5em'. In addition, these styles are not vendor-prefixed automatically, so you have to add vendor prefixes manually.
